c4ed482750e56ed977b4984cda66b35f8ae33ee4
antvega <antvega627@gmail.com>
2025-04-01 11:37:09 -0700

    Random computer move and displaying it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+type Move = "rock" | "paper" | "scissors";
+const MOVES: readonly Move[] = ["rock", "paper", "scissors"];
+
+function getRandomMove(): Move {
+  return MOVES[Math.floor(Math.random() * 3)];
+}
+getRandomMove();
 function App() {
+  const [computerMove, setComputerMove] = useState<Move>(getRandomMove());
   return (
     <div className="App">
-
+      <div>Computer Move: {computerMove}</div>
     </div>
   );
 }
