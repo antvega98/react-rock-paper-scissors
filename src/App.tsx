f8902f6faf69de69350647d58c054f1c47c49959
antvega <antvega627@gmail.com>
2025-04-05 14:45:20 -0700

    Reset game.

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,34 @@ type Move = (typeof MOVES)[number];
 function getRandomMove(): Move {
   return MOVES[Math.floor(Math.random() * 3)];
 }
+
 getRandomMove();
 function App() {
   const [computerMove, setComputerMove] = useState<Move>(getRandomMove());
   const [playerMove, setPlayerMove] = useState<Move | null>(null);
 
+  function newGame(){
+    setComputerMove(getRandomMove());
+    setPlayerMove(null);
+  }
+
   return (
     <div className="App">
-      <div>Computer Move: {computerMove}</div>
+      <div>Computer Move: {playerMove && computerMove }</div>
 
       <div>Player Move: {playerMove ?? "Make a move!"}</div>
-      <button onClick={()=>setPlayerMove("rock")}>Rock</button>
-      <button onClick={()=>setPlayerMove("paper")}>Paper</button>
-      <button onClick={()=>setPlayerMove("scissors")}>Scissors</button>
+      {
+        playerMove ? 
+        <>
+        <button onClick={()=> newGame()}>New game</button>
+        </>
+        : <>
+          <button onClick={()=>setPlayerMove("rock")}>Rock</button>
+          <button onClick={()=>setPlayerMove("paper")}>Paper</button>
+          <button onClick={()=>setPlayerMove("scissors")}>Scissors</button>
+        </>
+      }
+
     </div>
   );
 }
