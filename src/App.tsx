7f5d9734607c3e40688e90f55b75232ea0596a8a
antvega <antvega627@gmail.com>
2025-04-05 15:31:01 -0700

    Added winner.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,15 @@ function getRandomMove(): Move {
   return MOVES[Math.floor(Math.random() * 3)];
 }
 
-getRandomMove();
+function getWinner(moveA: Move, moveB: Move) : 'a' | 'b' | 'tie' {
+  if(moveA === moveB) return "tie";
+
+  const indexA = MOVES.indexOf(moveA);
+  const indexB = MOVES.indexOf(moveB);
+
+  return (indexA + 1 % MOVES.length === indexB ? "b" : "a");
+}
+
 function App() {
   const [computerMove, setComputerMove] = useState<Move>(getRandomMove());
   const [playerMove, setPlayerMove] = useState<Move | null>(null);
@@ -17,24 +25,29 @@ function App() {
     setComputerMove(getRandomMove());
     setPlayerMove(null);
   }
-
-  return (
-    <div className="App">
-      <div>Computer Move: {playerMove && computerMove }</div>
-
-      <div>Player Move: {playerMove ?? "Make a move!"}</div>
-      {
-        playerMove ? 
-        <>
-        <button onClick={()=> newGame()}>New game</button>
-        </>
-        : <>
+  if(playerMove == null) {
+    return (
+      <div className="App">
+          <div>Player Move: {playerMove ?? "Make a move!"}</div>
           <button onClick={()=>setPlayerMove("rock")}>Rock</button>
           <button onClick={()=>setPlayerMove("paper")}>Paper</button>
           <button onClick={()=>setPlayerMove("scissors")}>Scissors</button>
-        </>
-      }
+      </div>
+    )
+  }
 
+  const winnerText = {
+    a: "You win.",
+    b: "You lose.",
+    tie: "It's a tie."
+  }[getWinner(playerMove, computerMove)];
+
+  return (
+    <div className="App">
+      <div>Computer Move: {computerMove }</div>
+      <div>Your Move: {playerMove }</div>
+      <div>{winnerText}</div>
+      <button onClick={newGame}>New game</button>
     </div>
   );
 }
