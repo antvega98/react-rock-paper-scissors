76d19b32834a8b1e9e99c21fcffe0d6afa5498c9
antvega <antvega627@gmail.com>
2025-04-06 21:21:13 -0700

    Keeping score.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 const MOVES = ["rock", "paper", "scissors"] as const;
 type Move = (typeof MOVES)[number];
 
+type GameResults = { wins: number, losses: number, draws: number};
+
 function getRandomMove(): Move {
   return MOVES[Math.floor(Math.random() * 3)];
 }
@@ -20,18 +22,38 @@ function getWinner(moveA: Move, moveB: Move) : 'a' | 'b' | 'tie' {
 function App() {
   const [computerMove, setComputerMove] = useState<Move>(getRandomMove());
   const [playerMove, setPlayerMove] = useState<Move | null>(null);
+  const [{wins, losses, draws}, setGameResults] = useState<GameResults>({wins: 0, losses: 0, draws: 0});
 
   function newGame(){
     setComputerMove(getRandomMove());
     setPlayerMove(null);
   }
+
+  const handleMove = (move: Move) => {
+    setPlayerMove(move);
+    switch (getWinner(move, computerMove)) {
+      case "a": {
+        setGameResults({wins: wins + 1, losses, draws});
+        break;
+      }
+      case "b": {
+        setGameResults({wins: wins, losses: losses + 1, draws});
+        break;
+      }
+      case "tie" : {
+        setGameResults({wins, losses, draws: draws + 1});
+        break;
+      }
+    }
+  }
+
   if(playerMove == null) {
     return (
       <div className="App">
           <div>Player Move: {playerMove ?? "Make a move!"}</div>
-          <button onClick={()=>setPlayerMove("rock")}>Rock</button>
-          <button onClick={()=>setPlayerMove("paper")}>Paper</button>
-          <button onClick={()=>setPlayerMove("scissors")}>Scissors</button>
+          <button onClick={()=>handleMove("rock")}>Rock</button>
+          <button onClick={()=>handleMove("paper")}>Paper</button>
+          <button onClick={()=>handleMove("scissors")}>Scissors</button>
       </div>
     )
   }
@@ -44,9 +66,10 @@ function App() {
 
   return (
     <div className="App">
-      <div>Computer Move: {computerMove }</div>
-      <div>Your Move: {playerMove }</div>
+      <div>Computer Move: {computerMove}</div>
+      <div>Your Move: {playerMove}</div>
       <div>{winnerText}</div>
+      <div>Wins: {wins} - Losses: {losses} - Draws: {draws}</div>
       <button onClick={newGame}>New game</button>
     </div>
   );
